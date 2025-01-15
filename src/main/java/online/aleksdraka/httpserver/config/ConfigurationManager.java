801c52c5ff3ac534e5d6253d9f7c15cfb0627f92
Aleksandar Drakaliyski <theall3xander@gmail.com>
2025-01-15 16:48:16 +0000

    Added config

---
+++ src/main/java/online/aleksdraka/httpserver/config/ConfigurationManager.java
@@ -0,0 +1,19 @@
+﻿package online.aleksdraka.httpserver.config;
+
+public class ConfigurationManager {
+    private static ConfigurationManager myConfigurationManager;
+
+    private ConfigurationManager() {
+    }
+
+    public static ConfigurationManager getInstance() {
+        if (myConfigurationManager == null) {
+            myConfigurationManager = new ConfigurationManager();
+        }
+        return myConfigurationManager;
+    }
+
+    public void loadConfigurationFile(String filePath) {
+
+    }
+}
